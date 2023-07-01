dffbe0bb06c9ca7a9d0e2ee00db337662ecf6c29
Katlin Anderson <katlin.nbanderson@gmail.com>
2023-07-01 02:04:48 -0700

    Set up backend This was my first time using Django and took a fair bit of trial and error so there may be better ways I could have done this. Any feedback for improvement is welcome!

--- intsureview_be/intsureview_be/settings.py
+++ intsureview_be/intsureview_be/settings.py
@@ -31,17 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
     "intsureview_be.apps.api",
+    "rest_framework",
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -127,3 +130,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
